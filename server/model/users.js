b14c93561c091d6d9df73dfdb0991b196d7c1543
Doug Riblet <smashzen@users.noreply.github.com>
2016-09-21 14:48:18 -0500

    Auth module (#8)
    * AuthModule created, JWT added
    
    * Tweak AuthModule

--- server/model/users.js
+++ server/model/users.js
@@ -1,9 +1,15 @@
 var mongoose = require('mongoose');
 
 var usersSchema = new mongoose.Schema({
-  name: String,
+  firstName: String,
+  lastName: String,
+  email: {type: String, unique:true},
+  phone: String,
+  profilePicture: String,
   home_store: String,
-  rating: Number
-});
+  rating: Number,
+  profile: {facebook: String},
+  tokens: Array
+}, { timestamps: true });
 
 var Users = module.exports = mongoose.model('Users', usersSchema);
