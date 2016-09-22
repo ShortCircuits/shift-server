8c9f3ac522d525ff89c54cde28817098cb15c4e4
Jimmy Stevenson <jimmyastevenson@gmail.com>
2016-09-22 13:56:32 -0500

    changes for heroku deploy

---
+++ server/fb.js
@@ -0,0 +1,4 @@
+module.exports = {
+   FB_ID: "1169374106434305",
+   FB_SECRET: "b6f3ad61323c340bd64fe9d72a5a5ede"
+};
---
+++ server/ts.js
@@ -0,0 +1,3 @@
+module.exports = {
+TOKEN_SECRET: 'dougandchadsbougusjourneyjustbecausewearecool'
+};
