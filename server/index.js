fe2504b259a48fdf6b8f7e4eff7e21e8b19e1176
syqs <kristian.magda@gmail.com>
2016-10-03 11:09:02 -0500

    extended rateuser endpoint to update the voted property as well

--- server/index.js
+++ server/index.js
@@ -253,13 +253,14 @@ routes.patch('/rateuser', isAuthenticated, function(req, res){
             action[reps] = 1; 
 
             // if user requested is part of the pickup shift then procede with updating his reps;
-            Users.findOneAndUpdate({'_id': requester}, {$inc: action},function(err, items){
+            Users.findOneAndUpdate({'_id': requester}, {$set: {$inc: action, voted:true}},function(err, items){
               if (err) {
                 console.error(err.message);
                 res.status(404).send({error: err.message});
               }
               res.status(201).send("Successfuly added a rep");
-            })
+            });
+
           }
         }else{
            res.status(301).send("Shift has not yet happened, so no");
