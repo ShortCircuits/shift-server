ee5a5a44fe99445e156aa4ad0887bb76b0911e1d
Chad Griffis <cmg123@me.com>
2016-10-12 12:30:27 -0500

    adds the googleAuth auth.ctrl skeleton

--- server/controllers/auth.ctrl.js
+++ server/controllers/auth.ctrl.js
@@ -4,51 +4,68 @@ var AuthModule = require('../config/AuthModule');
 var TokenService = require('../config/TokenService');
 
 module.exports = {
-   facebookAuth: facebookAuth,
-   retrieveUser: retrieveUser,
-   generateToken: generateToken
+ facebookAuth: facebookAuth,
+ googleAuth: googleAuth,
+ retrieveUser: retrieveUser,
+ generateToken: generateToken
 };
 
 function facebookAuth(req, res, next) {
-   var options = {
-       code: req.body.code,
-       clientId: req.body.clientId,
-       redirectUri: req.body.redirectUri
-   };
+ var options = {
+   code: req.body.code,
+   clientId: req.body.clientId,
+   redirectUri: req.body.redirectUri
+ };
 
-   AuthModule.facebookAuthentication(options, function (err, response) {
-       if (err) return next({ err: err, status: 401 });
+ AuthModule.facebookAuthentication(options, function (err, response) {
+   if (err) return next({ err: err, status: 401 });
 
-       // for larger apps recommended to namespace req variables
-       req.authObject = response;
+   // for larger apps recommended to namespace req variables
+   req.authObject = response;
 
-       next();
-   });
+   next();
+ });
+}
+
+function googleAuth(req, res, next) {
+  var options = {
+    code: req.body.code,
+    clientId: req.body.clientId,
+    redirectUri: req.body.redirectUri
+  };
+
+  AuthModule.googleAuthentication(options, function (err, response) {
+    if (err) return next({ err: err, status: 401 });
+
+    req.authObject = response;
+
+    next();
+  });
 }
 
 function retrieveUser(req, res, next) {
-   if (!req.authObject) return next({ status: 401, err: 'User not found' });
+ if (!req.authObject) return next({ status: 401, err: 'User not found' });
 
-   var userToRetrieve = {
-       user: req.authObject.user,
-       type: req.authObject.type
-   };
+ var userToRetrieve = {
+   user: req.authObject.user,
+   type: req.authObject.type
+ };
 
-   AuthModule.createOrRetrieveUser(userToRetrieve, function (err, user) {
-       if (err || !user) return next({ status: 401, err: 'Error while fetching user' });
+ AuthModule.createOrRetrieveUser(userToRetrieve, function (err, user) {
+   if (err || !user) return next({ status: 401, err: 'Error while fetching user' });
 
-       req.user = user;
+   req.user = user;
 
-       next();
-   });
+   next();
+ });
 }
 
 function generateToken(req, res, next) {
-   TokenService.createToken({ user: req.user }, function (err, token) {
-       if (err) return next({ status: 401, err: 'User Validation failed' });
+ TokenService.createToken({ user: req.user }, function (err, token) {
+   if (err) return next({ status: 401, err: 'User Validation failed' });
 
-       req.genertedToken = token;
+   req.genertedToken = token;
 
-       next();
-   });
+   next();
+ });
 }
