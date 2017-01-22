c5bfcebd01e80c47bc3bacdb7695f39f960792b8
syqs <kristian.magda@gmail.com>
2017-01-22 15:08:32 -0600

    try 1 gauth

--- server/google.js
+++ server/google.js
@@ -1,4 +1,4 @@
 module.exports = {
-  GOOGLE_ID: "VJ1xfxIEKE2X2Jg7_jsLwlsJ",
-  GOOGLE_SECRET: "758774257882-1roptb8q4e5o1gd0g5a61pq99558nq6a.apps.googleusercontent.com"
+  GOOGLE_ID: "758774257882-1roptb8q4e5o1gd0g5a61pq99558nq6a.apps.googleusercontent.com",
+  GOOGLE_SECRET: "VJ1xfxIEKE2X2Jg7_jsLwlsJ"
 };
